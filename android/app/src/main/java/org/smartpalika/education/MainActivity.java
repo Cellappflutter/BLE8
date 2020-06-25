3e9610858b1fc6baa998bc20fedf550dfe69bd9d
pratik <testing.firebase99@gmail.com>
2020-06-25 11:41:03 +0545

    working

--- android/app/src/main/java/org/smartpalika/education/MainActivity.java
+++ android/app/src/main/java/org/smartpalika/education/MainActivity.java
@@ -2,6 +2,7 @@ package com.smartpalika.bleresults;
 
 
 import android.annotation.SuppressLint;
+import androidx.annotation.NonNull;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -14,51 +15,48 @@ import android.content.IntentFilter;
 import android.util.Log;
 import android.widget.Toast;
 
-import io.flutter.app.FlutterActivity;
+//import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-//import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
     private static  final String CHANNEL = "smartpalika_sms";
-
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            if (call.method.equals("sendSMS")) {
 
-        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodCallHandler() {
+                                final String myMessage = call.argument("symbol_no");
+                                android.util.Log.i("sent", "");
+                                android.util.Log.d("TAG", "myMessage");
 
-            @Override
-            public void onMethodCall(final MethodCall methodCall, final Result result) {
-                if (methodCall.method.equals("sendSMS")) {
+                                final Intent sms = new Intent(Intent.ACTION_VIEW);
+                                sms.setData(Uri.parse("smsto:"));
+                                sms.setType("vnd.android-dir/mms-sms");
 
-                    final String myMessage = methodCall.argument("symbol_no");
-                    android.util.Log.i("sent", "");
-                    android.util.Log.d("TAG", "myMessage");
-
-                    final Intent sms = new Intent(Intent.ACTION_VIEW);
-                    sms.setData(Uri.parse("smsto:"));
-                    sms.setType("vnd.android-dir/mms-sms");
-
-                    sms.putExtra("address","35001");
-                    sms.putExtra("sms_body","rslt "+myMessage);
-                    Toast.makeText(getApplicationContext(), myMessage, Toast.LENGTH_SHORT).show();
-                    Log.i("sent", "");
-                    try {
-                        startActivity(sms);
-                        Log.i("sent", "");
-                    } catch (final android.content.ActivityNotFoundException e) {
-                        Log.i("error sending","");
-                    }
+                                sms.putExtra("address","35001");
+                                sms.putExtra("sms_body","rslt "+myMessage);
+                                Toast.makeText(getApplicationContext(), myMessage, Toast.LENGTH_SHORT).show();
+                                Log.i("sent", "");
+                                try {
+                                    startActivity(sms);
+                                    Log.i("sent", "");
+                                } catch (final android.content.ActivityNotFoundException e) {
+                                    Log.i("error sending","");
+                                }
 
 
 
-                }
-
-            }
-        });
-
+                            }
+                        }
+                );
     }
+
 }
